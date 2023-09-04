7faacad6ad9fa2747d6c42c58d04a77f23a6ab4f
Ubuntu <gas_admin@2023-project.nkg23kbfm2zunbphr3a0i25gpg.syx.internal.cloudapp.net>
2023-09-04 10:27:38 +0000

    web add

--- webRoutes.js
+++ webRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
-router.use(express.static(path.join(__dirname, 'adminweb', 'build')));
+router.use(express.static(path.join(__dirname, 'web', 'build')));
 
 router.get('/', (req,res) => {
-  res.sendFile(path.join(__dirname, 'adminweb', 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'web', 'build', 'index.html'));
 });
 /*
 router.get('/', (req,res) => {
